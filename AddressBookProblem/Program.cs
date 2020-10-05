5cffeb0f8accff6ec3ba0875fdd962854d5179cf
SumitomShome <sumitomshome@gmail.com>
2020-10-05 17:28:27 +0530

    [Sumitom] Refactor: Commiting UC1-CreateContacts

--- AddressBookProblem/Program.cs
+++ AddressBookProblem/Program.cs
@@ -7,23 +7,7 @@ namespace AddressBookProblem
         {
             Console.WriteLine("Welcome to the address book problem!");
             AddressBookBuilder bookBuilder = new AddressBookBuilder();
-            Console.WriteLine("Enter first name");
-            String firstName = Console.ReadLine();
-            Console.WriteLine("Enter last name");
-            String lastName = Console.ReadLine();
-            Console.WriteLine("Enter address");
-            String address = Console.ReadLine();
-            Console.WriteLine("Enter city");
-            String city = Console.ReadLine();
-            Console.WriteLine("Enter state");
-            String state = Console.ReadLine();
-            Console.WriteLine("Enter zip");
-            String zip = Console.ReadLine();
-            Console.WriteLine("Enter phone No");
-            String phoneNumber = Console.ReadLine();
-            Console.WriteLine("Enter email");
-            String email = Console.ReadLine();
-            bookBuilder.AddContacts(firstName, lastName, address, city, state, zip, phoneNumber, email);
+            bookBuilder.AddContacts("Sumitom", "Shome", "Ruby", "Kolkata", "West Bengal", "700107", "9433430056", "sumitomshome@gmail.com");
             bookBuilder.DisplayContacts();
         }
     }
