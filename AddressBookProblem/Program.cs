5d38f3103e5895b924a14fe02d4534516021b8a3
SumitomShome <sumitomshome@gmail.com>
2020-11-02 00:43:50 +0530

    [Sumitom] Add: Ability to add multiple person to the address book

--- AddressBookProblem/Program.cs
+++ AddressBookProblem/Program.cs
@@ -1,16 +1,36 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 namespace AddressBookProblem
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the address book problem!");
             AddressBookBuilder bookBuilder = new AddressBookBuilder();
-            bookBuilder.AddContacts("Sumitom", "Shome", "Ruby", "Kolkata", "West Bengal", "700107", "9433430056", "sumitomshome@gmail.com");
-            bookBuilder.AddContacts("Aniket", "Mukherjee", "Salt Lake", "Kolkata", "West Bengal", "700064", "9073074916", "aniketmukherjee@gmail.com");
-            bookBuilder.AddContacts("Rahul", "Das", "Jadavpur", "Kolkata", "West Bengal", "700032", "9903309542", "rahuldas1996@gmail.com");
-            bookBuilder.AddContacts("Sayan", "Sarkar", "Birati", "Kolkata", "West Bengal", "700059", "8902644616", "sayansarkar@gmail.com");
+            Console.WriteLine("Welcome to the address book problem!");
+            Console.WriteLine("Please enter number of names you want to enter: ");
+            int input = Console.Read();
+            for (int i = 0; i <= input; i++)
+            {
+                Console.WriteLine("Enter first name: ");
+                String firstName = Console.ReadLine();
+                Console.WriteLine("Enter last name: ");
+                String lastName = Console.ReadLine();
+                Console.WriteLine("Enter address: ");
+                String address = Console.ReadLine();
+                Console.WriteLine("Enter city: ");
+                String city = Console.ReadLine();
+                Console.WriteLine("Enter state: ");
+                String state = Console.ReadLine();
+                Console.WriteLine("Enter zip: ");
+                String zip = Console.ReadLine();
+                Console.WriteLine("Enter phone number: ");
+                String phoneNumber = Console.ReadLine();
+                Console.WriteLine("Enter email: ");
+                String email = Console.ReadLine();
+                bookBuilder.AddContacts(firstName, lastName, address, city, state, zip, phoneNumber, email);
+                bookBuilder.DisplayContacts();
+            }
             bookBuilder.DisplayContacts();
             Console.WriteLine("Add the first name of the person whose contact you want to delete");
             String firstNameForDeleting = Console.ReadLine();
